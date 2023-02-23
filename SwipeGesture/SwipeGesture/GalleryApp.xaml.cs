e877db9c1c94908d45e2ee2f2cc7eed0f41648c4
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-02-23 21:25:00 +0700

    feat: init

---
+++ SwipeGesture/SwipeGesture/GalleryApp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwipeGesture
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GalleryApp : ContentPage
+	{
+		public GalleryApp ()
+		{
+			InitializeComponent ();
+		}
+		int state = 0, images = 5;
+		string[] image_array = new string[5]
+		{
+            "https://i.redd.it/2axq83mwivja1.png",
+            "https://i.redd.it/ith22br44bia1.jpg",
+            "https://i.redd.it/soloh4rvekja1.png",
+            "https://i.redd.it/5yqeuglgkrja1.jpg",
+            "https://i.redd.it/erh614se9sia1.jpg"
+        };
+		void swipeHandler(object sender, SwipedEventArgs e)
+		{
+			if(e.Direction == SwipeDirection.Left)
+			{
+				state = (state + 1 + images) % images;
+				img.Source = image_array[state];
+			}
+			else if(e.Direction == SwipeDirection.Right)
+			{
+				state = (state - 1 + images) % images;
+				img.Source = image_array[state];
+			}
+		}
+	}
+}
---
+++ SwipeGesture/SwipeGesture/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SwipeGesture
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void buttonHandler(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GalleryApp());
+        }
+    }
+}
